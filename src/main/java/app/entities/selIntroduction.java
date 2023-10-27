8c3c0007956fa35567b5d4be2ce38e51593c73df
oktaviadwiputripermatasari <oktaviadwiputri506@gmail.com>
2023-10-27 18:03:48 +0700

    add maven dependency

---
+++ src/main/java/app/entities/selIntroduction.java
@@ -0,0 +1,11 @@
+package app.entities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selIntroduction {
+    public static void main(String[] args) {
+        //invoking browser
+        WebDriver driver = new ChromeDriver();
+    }
+}
